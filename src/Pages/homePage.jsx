cbfc4fad82af2b754a1f5bc0eeec6d1a9cd38633
VijayManickam11 <vijaymanickam1101@gmail.com>
2025-07-05 15:38:26 +0530

    home page changes

--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../Styles/style.css";
 import bg_1 from "../Assets/images/myImage.png";
+import ChainImg from "../Assets/images/clean_transparent_chain.png";
 import AboutPage from "./About/about";
 import ResumePage from "./Resume/resume";
 import MySkillsPage from "./MySkills/mySkills";
@@ -12,10 +13,19 @@ import { FaLinkedin , FaInstagramSquare , FaGithubSquare } from "react-icons/fa"
 import { ImMobile } from "react-icons/im";
 import { AiOutlineMail } from "react-icons/ai";
 import "../Styles/style.css"
+import "../Styles/animetion.css"
 
 const HomePage = () => {
+   const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    
+    setAnimate(true);
+  }, []);
   return (    
     <>
+
+
     <section id="home-section" className="hero">
       <div className="home-slider  owl-carousel">
         <div className="slider-item ">
@@ -25,11 +35,29 @@ const HomePage = () => {
               className="row d-md-flex no-gutters slider-text align-items-end justify-content-end"
               data-scrollax-parent="true"
             >
-              <img
-                className="one-third js-fullheight order-md-last img"
-                src={bg_1}
-                alt="bg_1"
-              />
+<section className="hero-chain-section">
+  <div className={`hero-chain-wrapper ${animate ? "hero-chain-drop-swing" : ""}`}>
+    {/* Rope Image */}
+    <img src={ChainImg} alt="Rope" className="hero-chain-image" />
+
+    {/* ID Card */}
+    <div className="hero-chain-card hero-chain-hover-shake">
+      <img src={bg_1} alt="Vijay Manickam" className="hero-chain-profile" />
+      <div className="hero-chain-info">
+        <div class="masking-container">
+    <h1 class="masked-text">Vijay Manickam</h1>
+</div>
+        {/* <h2 className="hero-chain-name wave-text"><span>V</span><span>I</span><span>J</span><span>A</span><span>Y</span> <span>M</span><span>A</span><span>N</span><span>I</span><span>C</span><span>K</span><span>A</span><span>M</span></h2> */}
+        <p className="hero-chain-title">Frontend Developer</p>
+        <div className="hero-chain-footer">
+          <span>Dept. Developer</span>
+          <span>ID #000761</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
               <div className="one-forth d-flex  align-items-center">
                 <div className="text">
